2464b8d493b79cc437bab575ec26ec1455ed0e4f
Rez <codebahadur@gmail.com>
2025-03-14 00:26:44 +0900

    hello

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import ProductForm from "@/components/admin/product-form";
 import { Metadata } from "next";
 
@@ -6,13 +7,15 @@ export const metadata: Metadata = {
 }
 
 const CreateProductPage = () => {
-    return (<div>
-        <h2 className="h2-bold">create Product</h2>
-        <div className="my-8">
-
-            <ProductForm type='Create' />
+    return (
+        <div>
+            <h2 className="h2-bold">Create Product</h2>
+            <div className="my-8">
+                <Suspense fallback={<div>Loading Product Form...</div>}>
+                    <ProductForm type="Create" />
+                </Suspense>
+            </div>
         </div>
-    </div>
     );
 }
 
