2ea14b63e82bbd315888e2dfeb0b3fdd86d3bb45
Rez <codebahadur@gmail.com>
2025-03-17 15:52:24 +0900

    route changed

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,48 +2,32 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 import { updateOrderToPaid } from "@/lib/actions/order.actions";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-
 export async function POST(req: NextRequest) {
-  const payload = await req.text();
-  const signature = req.headers.get("stripe-signature") as string;
-
-  let event: Stripe.Event;
-
-  try {
-    event = stripe.webhooks.constructEvent(
-      payload,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET as string
-    );
-  } catch (err) {
-    return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
-  }
+  // Build the webhook event
+  const event = Stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get("stripe-signature") as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string
+  );
 
   // Check for successful payment
   if (event.type === "charge.succeeded") {
-    const charge = event.data.object as Stripe.Charge;
+    const { object } = event.data;
 
-    try {
-      await updateOrderToPaid({
-        orderId: charge.metadata.orderId,
-        paymentResult: {
-          id: charge.id,
-          status: "COMPLETED",
-          email_address: charge.billing_details.email!,
-          pricePaid: (charge.amount / 100).toFixed(2),
-        },
-      });
+    // Update order status
+    await updateOrderToPaid({
+      orderId: object.metadata.orderId,
+      paymentResult: {
+        id: object.id,
+        status: "COMPLETED",
+        email_address: object.billing_details.email!,
+        pricePaid: (object.amount / 100).toFixed(),
+      },
+    });
 
-      return NextResponse.json({
-        message: "updateOrderToPaid was successful",
-      });
-    } catch (error) {
-      return NextResponse.json(
-        { error: "Failed to update order status" },
-        { status: 500 }
-      );
-    }
+    return NextResponse.json({
+      message: "updateOrderToPaid was successful",
+    });
   }
 
   return NextResponse.json({
