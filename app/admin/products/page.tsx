a17a88f57106bd0481d2442d9a2d105b912f44d4
Rez <codebahadur@gmail.com>
2025-03-17 16:08:12 +0900

    hello

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,103 +1,99 @@
-import DeleteDialog from "@/components/shared/delete-dialog";
-import Pagination from "@/components/shared/pagination";
-import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { getAllProducts, deleteProduct } from "@/lib/actions/product.action";
-import { formatCurrency, formatId } from "@/lib/utils";
-import Link from "next/link";
+import Link from 'next/link';
+import { getAllProducts, deleteProduct } from '@/lib/actions/product.action';
+import { formatCurrency, formatId } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import Pagination from '@/components/shared/pagination';
+import DeleteDialog from '@/components/shared/delete-dialog';
+import { requireAdmin } from '@/lib/auth-guard';
 
-
-
-const AdminProductPage = async (props: {
+const AdminProductsPage = async (props: {
     searchParams: Promise<{
         page: string;
         query: string;
         category: string;
-    }>
+    }>;
 }) => {
-    const searchParams = await props.searchParams
-    const page = Number(searchParams.page) || 1
-    const searchText = searchParams.query || ''
-    const category = searchParams.category || ''
+    await requireAdmin();
+
+    const searchParams = await props.searchParams;
+
+    const page = Number(searchParams.page) || 1;
+    const searchText = searchParams.query || '';
+    const category = searchParams.category || '';
 
     const products = await getAllProducts({
         query: searchText,
         page,
         category,
-        // limit: 2 to test the limit 
-    })
+    });
 
-    console.log(products)
-
-    return (<div className="space-y-2">
-        <div className="flex-between">
-            <div className="flex items-center gap-3">
-                <h1 className="h2-bold">Products</h1>
-                {searchText && (
-                    <div>
-                        Filtered by <i>&quot;{searchText}&quot;</i>{' '}
-                        <Link href='/admin/products' >
-                            <Button variant='outline' size='sm'>Remove Filter</Button>
-                        </Link>
-                    </div>
-                )}
+    return (
+        <div className='space-y-2'>
+            <div className='flex-between'>
+                <div className='flex items-center gap-3'>
+                    <h1 className='h2-bold'>Products</h1>
+                    {searchText && (
+                        <div>
+                            Filtered by <i>&quot;{searchText}&quot;</i>{' '}
+                            <Link href='/admin/products'>
+                                <Button variant='outline' size='sm'>
+                                    Remove Filter
+                                </Button>
+                            </Link>
+                        </div>
+                    )}
+                </div>
+                <Button asChild variant='default'>
+                    <Link href='/admin/products/create'>Create Product</Link>
+                </Button>
             </div>
-            <Button asChild variant='default'>
-                <Link href='/admin/products/create'>Create Products</Link>
-            </Button>
-        </div>
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>
-                        ID
-                    </TableHead>
-                    <TableHead>
-                        NAME
-                    </TableHead>
-                    <TableHead className="text-right">
-                        PRICE
-                    </TableHead>
-                    <TableHead>
-                        CATEGORY
-                    </TableHead>
-                    <TableHead>
-                        STOCK
-                    </TableHead>
-                    <TableHead>
-                        RATING
-                    </TableHead>
-                    <TableHead className="w-[100px]">
-                        ACTIONS
-                    </TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {
-                    products.data.map((product) => (
+
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>ID</TableHead>
+                        <TableHead>NAME</TableHead>
+                        <TableHead className='text-right'>PRICE</TableHead>
+                        <TableHead>CATEGORY</TableHead>
+                        <TableHead>STOCK</TableHead>
+                        <TableHead>RATING</TableHead>
+                        <TableHead className='w-[100px]'>ACTIONS</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {products.data.map((product) => (
                         <TableRow key={product.id}>
                             <TableCell>{formatId(product.id)}</TableCell>
                             <TableCell>{product.name}</TableCell>
-                            <TableCell className="text-right">{formatCurrency(product.price)}</TableCell>
+                            <TableCell className='text-right'>
+                                {formatCurrency(product.price)}
+                            </TableCell>
                             <TableCell>{product.category}</TableCell>
                             <TableCell>{product.stock}</TableCell>
                             <TableCell>{product.rating}</TableCell>
-                            <TableCell className="flex gap-1">
-                                <Button>
+                            <TableCell className='flex gap-1'>
+                                <Button asChild variant='outline' size='sm'>
                                     <Link href={`/admin/products/${product.id}`}>Edit</Link>
                                 </Button>
                                 <DeleteDialog id={product.id} action={deleteProduct} />
                             </TableCell>
                         </TableRow>
-                    ))
-                }
-            </TableBody>
-        </Table>
-        {products.totalPages > 1 && (
-            <Pagination page={page} totalPages={products.totalPages} />
-        )}
+                    ))}
+                </TableBody>
+            </Table>
+            {products.totalPages > 1 && (
+                <Pagination page={page} totalPages={products.totalPages} />
+            )}
+        </div>
+    );
+};
 
-    </div>);
-}
-
-export default AdminProductPage;
+export default AdminProductsPage;
